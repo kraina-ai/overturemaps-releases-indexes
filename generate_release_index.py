48553f4ef52a02366e3967e12c58a80b0c22fdc7
Kamil Raczycki <kamil.raczycki@allegro.com>
2024-08-16 23:45:27 +0200

    feat: exit if release exists

--- generate_release_index.py
+++ generate_release_index.py
@@ -5,4 +5,6 @@ from overturemaestro.release_index import generate_release_index
 if __name__ == "__main__":
     release_version = sys.argv[1]
     print(release_version)
-    generate_release_index(release=release_version)
+    is_generated = generate_release_index(release=release_version)
+    if not is_generated:
+        sys.exit(1)
