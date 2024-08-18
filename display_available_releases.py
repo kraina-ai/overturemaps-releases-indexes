2894b0a0cab32d2b0e80912e3ff055b9be39d951
Kamil Raczycki <raczyckikamil@gmail.com>
2024-08-18 23:40:18 +0200

    chore: change minimal supported version

--- display_available_releases.py
+++ display_available_releases.py
@@ -2,6 +2,9 @@ import json
 from pathlib import Path
 from subprocess import call
 
+# This is the release with bbox with proper naming convention
+MINIMAL_SUPPORTED_RELEASE_VERSION = "2024-04-16-beta.0"
+
 if __name__ == "__main__":
     call(["python", "overturemaps_data/utils/fetch-releases-from-s3.py"])
     releases = json.loads(Path("releases.json").read_text())["releases"]
@@ -9,6 +12,6 @@ if __name__ == "__main__":
         release
         for release in releases
         if not (Path("release_indexes") / release).exists()
-        and not release.startswith("2023")
+        and release >= MINIMAL_SUPPORTED_RELEASE_VERSION
     ]
     print(f"{releases=}")
