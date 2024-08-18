7c73ad8bc4e62a2a4086b12c03035a9692bd8a9d
Kamil Raczycki <raczyckikamil@gmail.com>
2024-08-18 22:49:51 +0200

    feat: add releases fetching script

---
+++ display_available_releases.py
@@ -0,0 +1,13 @@
+import json
+from pathlib import Path
+from subprocess import call
+
+if __name__ == "__main__":
+    call(["python", "overturemaps_data/utils/fetch-releases-from-s3.py"])
+    releases = json.loads(Path("releases.json").read_text())["releases"]
+    releases = [
+        release
+        for release in releases
+        if not (Path("release_indexes") / release).exists()
+    ]
+    print(f"{releases=}")
