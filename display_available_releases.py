3263ddc5faab83968b637005a7b518a4b6a348d6
Kamil Raczycki <raczyckikamil@gmail.com>
2024-08-24 12:29:05 +0200

    feat: init repo

---
+++ display_available_releases.py
@@ -0,0 +1,18 @@
+import json
+from pathlib import Path
+from subprocess import call
+
+from overturemaestro.release_index import MINIMAL_SUPPORTED_RELEASE_VERSION
+
+if __name__ == "__main__":
+    call(["python", "overturemaps_data/utils/fetch-releases-from-s3.py"])
+    releases = json.loads(Path("releases.json").read_text())["releases"]
+    releases = [
+        release
+        for release in releases
+        if not (
+            Path("release_indexes") / release / "release_index_content.json"
+        ).exists()
+        and release >= MINIMAL_SUPPORTED_RELEASE_VERSION
+    ]
+    print(f"{releases=}")
---
+++ generate_release_index.py
@@ -0,0 +1,13 @@
+import sys
+
+from overturemaestro.release_index import _generate_release_index
+
+if __name__ == "__main__":
+    release_version = sys.argv[1]
+    theme_type_tuple = sys.argv[2]
+    theme_value, type_value = theme_type_tuple.split("|")
+    is_generated = _generate_release_index(
+        release=release_version, theme=theme_value, type=type_value
+    )
+    if not is_generated:
+        sys.exit(1)
