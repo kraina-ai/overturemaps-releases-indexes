c23b8f6853b312c87658192099efce2312de75b4
Kraina CI/CD <150701114+kraina-cicd@users.noreply.github.com>
2024-11-22 00:03:24 +0100

    fix: update generate_release_index.py

--- generate_release_index.py
+++ generate_release_index.py
@@ -7,7 +7,7 @@ if __name__ == "__main__":
     theme_type_tuple = sys.argv[2]
     theme_value, type_value = theme_type_tuple.split("|")
     is_generated = _generate_release_index(
-        release=release_version, theme=theme_value, type=type_value
+        release=release_version, theme=theme_value, type=type_value, index_location_path=f"release_indexes/{release_version}"
     )
     if not is_generated:
         sys.exit(1)
