c0fd287fe8e35683657a3111055a1b6ca6bea892
Kamil Raczycki <kamil.raczycki@allegro.com>
2024-11-24 11:51:28 +0100

    fix: change generate release index parameters

--- generate_release_index.py
+++ generate_release_index.py
@@ -7,7 +7,11 @@ if __name__ == "__main__":
     theme_type_tuple = sys.argv[2]
     theme_value, type_value = theme_type_tuple.split("|")
     is_generated = _generate_release_index(
-        release=release_version, theme=theme_value, type=type_value, index_location_path=f"release_indexes/{release_version}"
+        release=release_version,
+        theme=theme_value,
+        type=type_value,
+        index_location_path=f"release_indexes/{release_version}",
+        verbosity_mode="verbose",
     )
     if not is_generated:
         sys.exit(1)
